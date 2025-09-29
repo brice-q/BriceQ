28bedeee7e7390e95873c5ca1517fe4747197ce7
brice-q <21962912+brice-q@users.noreply.github.com>
2025-09-29 19:23:04 -0400

    fix: update version in package.json and improve styling in HomePage component

--- briceq-com/src/components/HomePage.tsx
+++ briceq-com/src/components/HomePage.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const HomePage: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-6">
-      <h1 className="text-6xl font-bold mb-50 text-[#0ea5e9] animate-pulse drop-shadow-[0_0_10px_rgba(255,255,255,0.3)]">BriceQ</h1>
+      <h1 className="text-6xl font-bold mb-40 text-[#0ea5e9] animate-pulse drop-shadow-[0_0_10px_rgba(255,255,255,0.3)]">BriceQ</h1>
 
       {/* Main content section with image and text */}
-      <div className="flex flex-col lg:flex-row items-center justify-center max-w-6xl mx-auto gap-10 mb-40">
+      <div className="flex flex-col lg:flex-row items-center justify-center max-w-6xl mx-auto gap-10 mb-25">
         {/* Left side - Image */}
         <div className="flex-1 flex justify-center">
           <img
@@ -18,7 +18,7 @@ const HomePage: React.FC = () => {
 
         {/* Right side - Welcoming message */}
         <div className="flex-1 text-center lg:text-center">
-          <h2 className="text-3xl font-bold mb-4 text-off-white">Welcome to My Portfolio!</h2>
+          <h2 className="text-3xl font-bold mb-4 text-off-white">Welcome to my Portfolio!</h2>
           <p className="text-lg text-gray-300 mb-10 leading-relaxed">
             Explore my projects and skills as a full-stack developer.
           </p>
